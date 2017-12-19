01ede4df2abb3c41d05a9972726f614f0e8703bc
apluser <apluser@blithe-stage.jhuapl.edu>
2017-12-18 19:31:47 -0500

    feat: first steps to customize client for stage

--- client/main.js
+++ client/main.js
@@ -3,18 +3,36 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.entities.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.entities.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.entities.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
+
+Template.scenes.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.scenes.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.scenes.events({
   'click button'(event, instance) {
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
